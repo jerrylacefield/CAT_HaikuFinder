23dd0db17441328c7d4c6e8be0c4e5b0f8b4fcc6
Jerry Lacefield <jerry.lacefield@gmail.com>
2017-11-04 17:40:02 -0500

    parser update

--- html_parser/parser.py
+++ html_parser/parser.py
@@ -1,1 +1,12 @@
-print("Hello, ConstantAlphaTex")
+from lxml import html
+import requests
+
+baseURL = "https://www.azlyrics.com"
+headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0' }
+
+page = requests.get(baseURL, headers=headers)
+tree = html.fromstring(page.content)
+
+alphaLink = tree.xpath('//a[@class="btn btn-menu"]/text()')
+
+print('Link: ', alphaLink)
