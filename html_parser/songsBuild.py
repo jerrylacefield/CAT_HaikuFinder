508a0876a1bd6e8ae9054d9123133e950fb9aaef
Jerry Lacefield <jerry.lacefield@gmail.com>
2017-11-05 08:11:06 -0600

    songsBuild ip address change

--- html_parser/songsBuild.py
+++ html_parser/songsBuild.py
@@ -4,7 +4,7 @@ import songsParser as songs
 
 def getNextLink():
     print("Attempting to get the link...")
-    resp = requests.get("http://172.25.21.208:8080/getLink", params={'type':'1'})
+    resp = requests.get("http://104.198.254.12:8080/getLink", params={'type':'1'})
     print("Link has been gotten...")
 
     data = resp.json()
@@ -12,10 +12,10 @@ def getNextLink():
 
 
 def updateSongsByArtist(key, value1, value2):
-	send = requests.post("http://172.25.21.208:8080/updateData", params={'link':key, 'data':value1, 'data':value2, 'type':'1'})
+	send = requests.post("http://104.198.254.12:8080/updateData", params={'link':key, 'data':value1, 'data':value2, 'type':'1'})
 
 def finishSongs(link):
-	send = requests.post("http://172.25.21.208:8080/finished", params={'link':link, 'type':'1'})
+	send = requests.post("http://104.198.254.12:8080/finished", params={'link':link, 'type':'1'})
 
 baseURL = "https://www.azlyrics.com"
 headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0' }
