dd26153454f065e96187c62e3a050db52f63513e
Ian Fennen <ifennen@ymail.com>
2017-11-04 20:56:31 -0500

    Prettified the html parsing

--- html_parser/parser.py
+++ html_parser/parser.py
@@ -1,12 +1,18 @@
-from lxml import html
+from bs4 import BeautifulSoup as bs
 import requests
 
-baseURL = "https://www.azlyrics.com"
+baseURL = "https://www.azlyrics.com/a.html"
 headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0' }
 
 page = requests.get(baseURL, headers=headers)
-tree = html.fromstring(page.content)
 
-alphaLink = tree.xpath('//a[@class="btn btn-menu"]/text()')
+soup = bs(page.text, 'html.parser')
+#soup = soup.encode('utf8').replace("<br>", "<br/>")
+source = soup.prettify()
 
-print('Link: ', alphaLink)
+source = source.encode('utf8').replace("<br>", "<br/>").replace("</br>", "").replace("\t","")
+
+
+
+
+#print('Link: ', alphaLink)
